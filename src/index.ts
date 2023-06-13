1031600732357e096fc809c0326dbcb9c271b7ed
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-06-12 19:16:56 -0700

    Add map and implement search result

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,17 @@ const searchBar = <HTMLInputElement>document.getElementById("searchBar");
 const searchBtn = <HTMLButtonElement>document.getElementById("searchBtn");
 const API_KEY: string = "at_belSPVglRs4FaR8mRJ3GMqX1kwU3E";
 let API_URI: string = "https://geo.ipify.org/api/v2/country,city";
-const searchResults = document.querySelectorAll('.header__search-result')
+const searchResults = document.querySelectorAll(".header__search-result");
+let map;
+console.log(map);
 
 // Global Variables
 let searchInput: string = "";
 const ipAddressRegex = /[0-255].[0-255]./;
 let mapCoor: number[];
 
-
 // Utility functions
-function getMap(coor: number[]) {
-  var map = L.map("map");
-  map.setView(coor, 13);
-}
-
-function displaySearchResults() {
-    
-}
-
-//Attach Event Listeners
-searchBar.addEventListener("keyup", (e): void => {
-  searchInput = e.target?.value;
-});
-
-searchBtn.addEventListener("click", () => {
+function init() {
   const requestType = ipAddressRegex.test(searchInput) ? "ipAddress" : "domain";
 
   console.log(requestType);
@@ -44,10 +31,61 @@ searchBtn.addEventListener("click", () => {
       const data = await res.json();
       console.log(data);
       mapCoor = [data.location.lat, data.location.lng];
-      getMap(mapCoor)
+      getMap(mapCoor);
+      displaySearchResults(data);
     } catch (error) {
       console.log(error);
     }
   }
   getData();
+}
+
+function getMap(coor: number[]) {
+  map = L.map("map", {
+    center: coor,
+    zoom: 18,
+    zoomControl: false,
+  });
+  L.tileLayer(
+    "https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=MXsJ5e0B7SZOvcTRu5c3",
+    {
+      attribution: `<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>
+    <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>`,
+    }
+  ).addTo(map);
+
+  let icon = L.icon({
+    iconUrl: "../images/icon-location.svg",
+    iconSize: [30, 38],
+    iconAnchor: [22, 94],
+  });
+  let marker: any = L.marker(coor, {
+    icon: icon,
+  });
+
+  marker.addTo(map);
+
+  //   map.setView(coor, 13);
+}
+
+function displaySearchResults(data: any) {
+  searchResults.forEach((result) => {
+    const searchResultData = <HTMLParagraphElement>(
+      result.querySelector(".search-result-data")
+    );
+
+    searchResultData.innerText =
+      result.id === "city" || result.id === "timezone"
+        ? data["location"][result.id]
+        : data[result.id];
+  });
+}
+
+//Attach Event Listeners
+searchBar.addEventListener("keyup", (e): void => {
+  searchInput = e.target?.value;
 });
+
+window.addEventListener("load", init);
+
+searchBtn.addEventListener("click", init);
